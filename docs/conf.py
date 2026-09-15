170f2e378cccecd87a9fe7a76ae119d176a15ef9
Evan <ejhingm@comcast.net>
2026-09-15 15:32:01 -0400

    added more detail

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,26 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'ImagineRIT-WBC'
-copyright = '2026, Evan Hughes'
-author = 'Evan Hughes'
-release = '1.0.1'
+project = "ImagineRIT-WBC"
+copyright = "2026, Evan Hughes"
+author = "Evan Hughes"
+release = "1.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx_rtd_theme",
+]
 
-templates_path = ['_templates']
+html_theme = "sphinx_rtd_theme"
+
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
