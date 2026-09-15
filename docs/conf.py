82be958b6fdd933b3e370d6f1e7b4223c8aa6dfd
ejh2540 <ejh2540@OH1380-09>
2026-09-15 14:52:23 -0400

    updating version

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'ImagineRIT-WBC'
 copyright = '2026, Evan Hughes'
 author = 'Evan Hughes'
-release = '1.0'
+release = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
